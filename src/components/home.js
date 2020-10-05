7c23893d55fc1c5c0859f49bf9f48e050a4ee864
Diego Simonelli <dassimonelli@gmail.com>
2020-10-05 17:40:12 -0400

    added some links to home

--- src/components/home.js
+++ src/components/home.js
@@ -15,13 +15,32 @@ export default class Home extends Component {
             I used Mozilla Hubs to demonstrate a unique experience to new users
             in order to convince them to help the organization.
           </p>
+
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Experience preview on Youtube!
+            Experience preview on Youtube link here!
+          </a>
+
+          <a
+            className="App-link"
+            href="https://youtu.be/R3cdhLIDWeQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Guest trying out the experience link here!
+          </a>
+
+          <a
+            className="App-link"
+            href="https://hub.link/5pyxSiy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VR Kids Organization Hubs Room link here!
           </a>
         </header>
       </>
